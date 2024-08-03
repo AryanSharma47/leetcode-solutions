d31852d7e673f57d5c138fe799ea54a9624be938
Aryan <aryansharma.hs07@gmail.com>
2024-08-03 17:00:59 +0530

    New Commit

---
+++ 1460.MakeTwoArraysEqualByReversingSubarrays.cpp
@@ -0,0 +1,15 @@
+//Simple Approach
+class Solution {
+public:
+    bool canBeEqual(vector<int>& target, vector<int>& arr) {
+        sort(target.begin(),target.end());
+        sort(arr.begin(),arr.end());
+
+        for(int i=0;i<arr.size();i++)
+        {
+            if(arr[i] != target[i])
+            return false;
+        }
+        return true;
+    }
+};
